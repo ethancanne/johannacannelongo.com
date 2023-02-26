bfcd7e48ca758909187f3a7088c2afa1a30b42ab
Ethan Cannelongo <ethancannelongo@gmail.com>
2023-02-25 19:15:29 -0600

    Added Preview image

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -14,6 +14,7 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:image" content="/images/johanna.jpeg" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
