57f82c31d92553ff04766edc348d282821e01338
Ethan Cannelongo <ethancannelongo@gmail.com>
2023-02-25 19:08:09 -0600

    Revised Description, optimized photos, and added favicon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,7 +20,7 @@ export const head: DocumentHead = {
     {
       name: 'description',
       content:
-        "Johanna is a Jesus follower, wife, mother of eight (four adopted), and writer from Maryland. She created a beautiful garden, 'Gianna's Garden of Hope,' in memory of her daughter who passed away at age six. Johanna is passionate about helping families navigate grief and trauma, and founded the nonprofit Giashope.org to support orphans and the vulnerable.",
+        'Johanna is a Jesus follower, wife, mother of eight (four adopted), and writer from Maryland. Johanna is passionate about helping families navigate grief and trauma, and founded the nonprofit Giashope.org to support orphans and the vulnerable.',
     },
   ],
 };
