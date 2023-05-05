200e136147cdc0ab5938f730d25e98f08d310cad
Ethan Cannelongo <ethancannelongo@gmail.com>
2023-05-05 13:19:35 -0400

    optimized images and changed youtube embed

--- src/views/book/views/3-trailer/Trailer.view.tsx
+++ src/views/book/views/3-trailer/Trailer.view.tsx
@@ -5,15 +5,19 @@ export default component$(() => {
   return (
     <div class={styles.container}>
       <h1 class={styles.title}>Book Trailer</h1>
-      <div class={styles.trailerContainer}>
-        {/* <iframe
-          width="560"
-          height="315"
-          class={styles.trailer}
-          src="https://www.youtube.com/embed/yT80zvLa3W4"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+
+      <div
+        class={styles.trailerContainer}
+        style="height: 0px; overflow: hidden; padding-top: 56.25%; position: relative; width: 100%;"
+      >
+        <iframe
+          style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;"
+          src="https://tube.rvere.com/embed?v=yT80zvLa3W4"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-        ></iframe> */}
+        ></iframe>
       </div>
     </div>
   );
