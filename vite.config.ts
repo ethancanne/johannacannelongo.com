ce341c789c0f08ca617bc10ee5c6e4241d9016b9
Ethan Cannelongo <ethancannelongo@gmail.com>
2023-05-05 12:48:34 -0400

    Optimized images

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,33 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import vsharp from 'vite-plugin-vsharp';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      vsharp({
+        includePublic: [],
+        excludePublic: [],
+        exclude: [],
+        '.jpg': {
+          quality: 5,
+        },
+        '.jpeg': {
+          quality: 5,
+        },
+        '.png': {
+          quality: 5,
+          palette: true,
+        },
+        '.webp': {
+          lossless: true,
+        },
+      }),
+    ],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
